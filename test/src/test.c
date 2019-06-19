fac23b5416aa43ac50f6d46fff5b5daf6504e9d3
Yi Yao <leaf_yao8621@hotmail.com>
2019-06-19 18:13:35 +0800

    fixing gitignore

--- test/src/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "../../emulator/emulator.h"
-#include "../../assembler/assembler.h"
-
-int main(void) {
-    assemble("fibb.asm", "fibb.obj", "fibb.dmp");
-    load("fibb.obj");
-    run(0);
-}
