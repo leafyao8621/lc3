39176fa601502a873dba49632375d86b4ea17604
Yi Yao <leaf_yao8621@hotmail.com>
2019-07-06 09:23:32 +0800

    added code generation tools

---
+++ misc/gen_code.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+char *strs[] = {
+    "ADD",
+    "AND",
+    "BR",
+    "BRN",
+    "BRZ",
+    "BRP",
+    "BRNZ",
+    "BRZP",
+    "BRNP",
+    "BRNZP",
+    "JMP",
+    "JSR",
+    "JSRR",
+    "LD",
+    "LDI",
+    "LDR",
+    "LEA",
+    "NOT",
+    "ST",
+    "STI",
+    "STR",
+    "TRAP",
+    0
+};
+
+char buf[8];
+int main() {
+    for (char** i = strs; *i; i++) {
+        memset(buf, 0, 8);
+        for (char j = 0, *k = buf, *l = *i; j < 8 && *l; j++,
+                                                         *(k++) = *(l++));
+        printf("case 0x%lx: //%s\n"
+               "    *out = %s;\n"
+               "    return 1;\n",
+               *(uint64_t*)buf, buf, buf);
+    }
+}
---
+++ misc/gen_code1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+char *strs[] = {
+    ".ORIG",
+    ".END",
+    ".FILL",
+    ".STRINGZ",
+    ".BLKW",
+    0
+};
+
+char buf[8];
+int main() {
+    for (char** i = strs; *i; i++) {
+        memset(buf, 0, 8);
+        for (char j = 0, *k = buf, *l = *i; j < 8 && *l; j++,
+                                                         *(k++) = *(l++));
+        printf("case 0x%lx: //%s\n"
+               "    *out = %s;\n"
+               "    return 1;\n",
+               *(uint64_t*)buf, buf, buf + 1);
+    }
+}
